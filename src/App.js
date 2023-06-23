64e986cf33823ce90d9dd69b96b66d33811dc8e1
Paris Yee <paris.yee@gmail.com>
2023-06-22 21:45:02 -0400

    attempting to configure the deploy workflow

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <p>
+        We make movies.
+      </p>
     </div>
   );
 }
